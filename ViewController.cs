75a902cc26174b5767b8be072ca2c7550e17c1a2
tomomo-s <tomomo_0326@yahoo.co.jp>
2017-06-06 08:28:25 -0700

    Add files via upload

---
+++ ViewController.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using AppKit;
+using Foundation;
+using System.IO;
+
+namespace TestReadWrite
+{
+    public partial class ViewController : NSViewController
+    {
+        public ViewController(IntPtr handle) : base(handle)
+        {
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+
+            // Do any additional setup after loading the view.
+        }
+
+        public override NSObject RepresentedObject
+        {
+            get
+            {
+                return base.RepresentedObject;
+            }
+            set
+            {
+                base.RepresentedObject = value;
+                // Update the view, if already loaded.
+            }
+        }
+        // ViewController.designer.csの定義を記述
+        partial void btnRead(Foundation.NSObject sender)
+        {
+			// ファイルの存在チェック
+			bool bRet = NSFileManager.DefaultManager.FileExists(textReadValue.StringValue);
+            if (bRet)
+            {
+                // ファイル拡張子は、.txtのみにする
+                var extension = Path.GetExtension(textReadValue.StringValue);
+                if(extension == ".txt")
+                {
+					// ファイル読み込み
+					var readdata = File.ReadAllText(textReadValue.StringValue);
+
+					if (readdata != null)
+					{
+						// TextViewに読み込みデータ表示
+						textviewReadValue.Value = readdata.ToString();
+					}
+					else
+					{
+						// TextViewにエラー表示
+						textviewReadValue.Value = "Not Data...";
+					} 
+                }
+                else
+                {
+					// TextViewにエラー表示
+					textviewReadValue.Value = "FileExtension is .txt Only...";
+				}
+            }
+            else
+            {
+				// TextViewにエラー表示
+				textviewReadValue.Value = "File Not Found...";         
+            }
+        }
+        // ViewController.designer.csの定義を記述
+        partial void btnWrite(Foundation.NSObject sender)
+        {
+            var extension = Path.GetExtension(textWriteValue.StringValue);
+            if (extension == ".txt")
+            {
+                if(textviewWriteValue.Value != null)
+                {
+                    var filepath = Path.GetDirectoryName(textWriteValue.StringValue);
+                    // ディレクトリ確認
+                    bool bRet = NSFileManager.DefaultManager.FileExists(filepath);
+                    if(bRet)
+                    {
+                        File.WriteAllText(textWriteValue.StringValue, textviewWriteValue.Value);
+                    }
+                    else
+                    {
+						// TextViewにエラー表示
+						textviewWriteValue.Value = "Directory Not Found...";           
+                    }
+                }
+                else
+                {
+					// TextViewにエラー表示
+					textviewWriteValue.Value = "Not Data...";
+                }
+            }
+			else
+			{
+				// TextViewにエラー表示
+				textviewWriteValue.Value = "FileExtension is .txt Only...";
+			}
+        }
+    }
+}
---
+++ ViewController.designer.cs
@@ -0,0 +1,88 @@
+﻿// WARNING
+//
+// This file has been generated automatically by Visual Studio to store outlets and
+// actions made in the UI designer. If it is removed, they will be lost.
+// Manual changes to this file may not be handled correctly.
+//
+using Foundation;
+using System.CodeDom.Compiler;
+
+namespace TestReadWrite
+{
+    [Register ("ViewController")]
+    partial class ViewController
+    {
+        [Outlet]
+        AppKit.NSTextField textReadPath { get; set; }
+
+        [Outlet]
+        AppKit.NSTextField textReadValue { get; set; }
+
+        [Outlet]
+        AppKit.NSTextView textviewReadValue { get; set; }
+
+        [Outlet]
+        AppKit.NSTextView textviewWriteValue { get; set; }
+
+        [Outlet]
+        AppKit.NSTextField textWritePath { get; set; }
+
+        [Outlet]
+        AppKit.NSTextField textWriteValue { get; set; }
+
+        [Outlet]
+        AppKit.NSScrollView viewReadValue { get; set; }
+
+        [Outlet]
+        AppKit.NSScrollView viewWriteValue { get; set; }
+
+        [Action ("btnRead:")]
+        partial void btnRead (Foundation.NSObject sender);
+
+        [Action ("btnWrite:")]
+        partial void btnWrite (Foundation.NSObject sender);
+        
+        void ReleaseDesignerOutlets ()
+        {
+            if (textReadPath != null) {
+                textReadPath.Dispose ();
+                textReadPath = null;
+            }
+
+            if (textReadValue != null) {
+                textReadValue.Dispose ();
+                textReadValue = null;
+            }
+
+            if (textWritePath != null) {
+                textWritePath.Dispose ();
+                textWritePath = null;
+            }
+
+            if (textWriteValue != null) {
+                textWriteValue.Dispose ();
+                textWriteValue = null;
+            }
+
+            if (viewReadValue != null) {
+                viewReadValue.Dispose ();
+                viewReadValue = null;
+            }
+
+            if (viewWriteValue != null) {
+                viewWriteValue.Dispose ();
+                viewWriteValue = null;
+            }
+
+            if (textviewReadValue != null) {
+                textviewReadValue.Dispose ();
+                textviewReadValue = null;
+            }
+
+            if (textviewWriteValue != null) {
+                textviewWriteValue.Dispose ();
+                textviewWriteValue = null;
+            }
+        }
+    }
+}
